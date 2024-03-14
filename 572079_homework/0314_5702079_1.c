4eec682a2191306a0918c0ed87288ccde9030c91
효상 이 <qhsry123@naver.com>
2024-03-14 11:00:36 +0900

    소수의 합_0314_이효상

--- 572079_homework/0314_5702079_1.c
+++ 572079_homework/0314_5702079_1.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <time.h>
+
+
+int main(void) {
+
+
+	clock_t start = clock();
+	double duration;
+	
+
+	int result = 0;
+	int a = 0;
+	
+		for (int i = 2; i <= 100; i++)
+		{
+			for (a = 2; a < i; a++) {
+				if (i % a == 0)break;
+
+			}
+			if (a == i)
+				result += a;
+
+		}
+		clock_t stop = clock();
+	printf("소수의 합은 %d \n", result);
+
+	
+	duration = (double)(stop - start/ CLOCKS_PER_SEC);
+	printf("알고리즘 수행 시간은 %f초입니다\n", duration);
+
+}
